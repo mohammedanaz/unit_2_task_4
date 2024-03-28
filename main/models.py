0941828130f787e5a86101cecbb8af0a47343ecf
mohammedanaz <mohammedanaz@gmail.com>
2024-03-28 11:17:08 +0530

    save() method override to delete cache once a new prod added

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.cache import cache
 
 
 class Products(models.Model):
@@ -10,4 +11,8 @@ class Products(models.Model):
     def __str__(self):
         return self.prod_name
     
+    ################### Overriding save() method of models.Model to delete cache once a new prod added##############
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs) # this ensures that usual save() method of parent also executed
+        cache.delete('home_cache') # this deletes the cache so that home view will load cache again once a prod is added
 
